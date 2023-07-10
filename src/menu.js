85c7b3091dc704e7cd66d472c5dcb98d13a27a01
emShalaby <galaxian.dev@gmail.com>
2023-07-10 15:56:58 +0300

    assign each menu item an id based on its order

--- src/menu.js
+++ src/menu.js
@@ -11,6 +11,13 @@ export function menuPage() {
   const h3 = document.createElement("h3");
   const itemContainer1 = document.createElement("div");
 
+  bestSelling.appendChild(h3);
+  bestSelling.appendChild(itemContainer1);
+  header.appendChild(title);
+  content.appendChild(header);
+  main.appendChild(bestSelling);
+  content.appendChild(main);
+
   header.id = "header";
   main.id = "main";
   itemContainer1.id = "best-selling";
@@ -18,15 +25,12 @@ export function menuPage() {
   title.innerText = "Menu";
   h3.textContent = "Best Selling";
 
-  bestSelling.appendChild(h3);
   for (let i = 0; i < 2; i++) {
     const div = document.createElement("div");
     div.classList.add("menu-item");
+    div.id = "item" + i;
     itemContainer1.appendChild(div);
   }
-  bestSelling.appendChild(itemContainer1);
-  header.appendChild(title);
-  content.appendChild(header);
-  main.appendChild(bestSelling);
-  content.appendChild(main);
+  const item0 = document.querySelector("#item0");
+  const item1 = document.querySelector("#item1");
 }
