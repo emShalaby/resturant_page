340e036231176236d2b34e1186d30984392268a3
emShalaby <galaxian.dev@gmail.com>
2023-07-11 02:54:27 +0300

    finished project and dont plan to work on the design anymore

--- src/contacts.js
+++ src/contacts.js
@@ -7,12 +7,16 @@ export function contactsPage() {
   const header = document.createElement("div");
   const title = document.createElement("h1");
   const main = document.createElement("div");
+  const h3 = document.createElement("h3");
 
   header.id = "header";
   main.id = "main";
+  main.appendChild(h3);
+  h3.textContent = "CONTACT US AT 055-555-555";
 
   title.innerText = "Contacts";
 
   header.appendChild(title);
   content.appendChild(header);
+  content.appendChild(main);
 }
