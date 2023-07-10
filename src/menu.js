5e5621ec4ea864608000d1a142845acecc8cfd27
emShalaby <galaxian.dev@gmail.com>
2023-07-10 15:29:07 +0300

    add a best selling div to menu module

--- src/menu.js
+++ src/menu.js
@@ -7,12 +7,16 @@ export function menuPage() {
   const header = document.createElement("div");
   const title = document.createElement("h1");
   const main = document.createElement("div");
-
+  const bestSelling = document.createElement("div");
+  const h3 = document.createElement("h3");
   header.id = "header";
   main.id = "main";
 
   title.innerText = "Menu";
-
+  h3.textContent = "Best Selling";
+  bestSelling.appendChild(h3);
   header.appendChild(title);
   content.appendChild(header);
+  main.appendChild(bestSelling);
+  content.appendChild(main);
 }
