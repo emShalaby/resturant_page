7aee6a01dcbf940c37fde0ee0e85772472b84c7b
emShalaby <galaxian.dev@gmail.com>
2023-07-10 14:13:17 +0300

    add introductry text to homepage

--- src/home.js
+++ src/home.js
@@ -6,12 +6,18 @@ export function homePage() {
   const header = document.createElement("div");
   const title = document.createElement("h1");
   const main = document.createElement("div");
-
+  const intro = document.createElement("div");
+  const p = document.createElement("p");
   header.id = "header";
   main.id = "main";
 
   title.innerText = "SHAWARMA METER";
 
+  p.innerHTML =
+    "Shawarma Meter provides you delicious shawarma with competetive prices and amazing offers .<br>Order now for free delivery!";
+
   header.appendChild(title);
   content.appendChild(header);
+  intro.appendChild(p);
+  content.appendChild(p);
 }
