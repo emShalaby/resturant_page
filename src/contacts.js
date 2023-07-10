e22b52f68c1d3287fe9cfd7c771228aa88b42816
emShalaby <galaxian.dev@gmail.com>
2023-07-10 10:34:02 +0300

    add div with id as main to the 3 modules

--- src/contacts.js
+++ src/contacts.js
@@ -1,11 +1,15 @@
 export function contactsPage() {
   if (document.querySelector("#header"))
     document.querySelector("#header").remove();
+  if (document.querySelector("#main")) document.querySelector("#main").remove();
 
   const content = document.querySelector("#content");
   const header = document.createElement("div");
   const title = document.createElement("h1");
+  const main = docuemnt.createElement("div");
+
   header.id = "header";
+  main.id = "main";
 
   title.innerText = "Contacts";
 
--- src/home.js
+++ src/home.js
@@ -1,10 +1,14 @@
 export function homePage() {
   if (document.querySelector("#header"))
     document.querySelector("#header").remove();
+  if (document.querySelector("#main")) document.querySelector("#main").remove();
   const content = document.querySelector("#content");
   const header = document.createElement("div");
   const title = document.createElement("h1");
+  const main = docuemnt.createElement("div");
+
   header.id = "header";
+  main.id = "main";
 
   title.innerText = "RESTURANT TITLE";
 
