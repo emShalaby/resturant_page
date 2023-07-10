a851ce4a48b0530217f4a07fabef69fdf105055f
emShalaby <galaxian.dev@gmail.com>
2023-07-10 15:49:59 +0300

    make the best selling section have 2 cards

--- src/menu.js
+++ src/menu.js
@@ -9,12 +9,22 @@ export function menuPage() {
   const main = document.createElement("div");
   const bestSelling = document.createElement("div");
   const h3 = document.createElement("h3");
+  const itemContainer1 = document.createElement("div");
+
   header.id = "header";
   main.id = "main";
+  itemContainer1.id = "best-selling";
 
   title.innerText = "Menu";
   h3.textContent = "Best Selling";
+
   bestSelling.appendChild(h3);
+  for (let i = 0; i < 2; i++) {
+    const div = document.createElement("div");
+    div.classList.add("menu-item");
+    itemContainer1.appendChild(div);
+  }
+  bestSelling.appendChild(itemContainer1);
   header.appendChild(title);
   content.appendChild(header);
   main.appendChild(bestSelling);
