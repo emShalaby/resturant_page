1638f14906a437c06e4de9f08a1f968db529d1e8
emShalaby <galaxian.dev@gmail.com>
2023-07-10 15:58:43 +0300

    change webpack config to handle images

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   module: {
-    rules: [{ test: /\.css$/i, use: ["style-loader", "css-loader"] }],
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      { test: /\.(png|svg|jpg|jpeg|gif)$/i, type: "asset/resource" },
+    ],
   },
 };
