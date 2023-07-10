f0f4342ccfdc5531412c8b7bcb7083c9c18abb85
emShalaby <galaxian.dev@gmail.com>
2023-07-10 18:59:29 +0300

    make a function that generates menu items

--- src/menu.js
+++ src/menu.js
@@ -11,52 +11,55 @@ export function menuPage() {
   const main = document.createElement("div");
   const bestSelling = document.createElement("div");
   const h3 = document.createElement("h3");
-  const itemContainer1 = document.createElement("div");
+  const bestSellingItemsContainer = document.createElement("div");
 
-  bestSelling.appendChild(h3);
-  bestSelling.appendChild(itemContainer1);
   header.appendChild(title);
   content.appendChild(header);
+
+  bestSelling.appendChild(h3);
+  bestSelling.appendChild(bestSellingItemsContainer);
+
   main.appendChild(bestSelling);
   content.appendChild(main);
 
   header.id = "header";
   main.id = "main";
-  itemContainer1.id = "best-selling-items";
+  bestSellingItemsContainer.id = "best-selling-items";
   bestSelling.id = "best-selling";
 
   title.innerText = "Menu";
   h3.textContent = "Best Selling";
 
-  for (let i = 0; i < 2; i++) {
-    const div = document.createElement("div");
-    div.classList.add("menu-item");
-    div.id = "item" + i;
-    itemContainer1.appendChild(div);
+  function createItem(img, price, name, description, sectionID) {
+    const itemImage = new Image();
+    const itemPrice = document.createElement("p");
+    const itemName = document.createElement("h4");
+    const itemDescription = document.createElement("p");
+    const itemSection = document.querySelector("#" + sectionID);
+    const text = document.createElement("div");
+    const item = document.createElement("div");
+    item.classList.add("menu-item");
+    itemPrice.textContent = price;
+    itemPrice.classList.add("price");
+    itemName.textContent = name;
+    itemDescription.textContent = description;
+    itemImage.src = img;
+
+    item.appendChild(itemImage);
+    item.appendChild(text);
+    item.appendChild(itemPrice);
+
+    text.appendChild(itemName);
+    text.appendChild(itemDescription);
+    itemSection.appendChild(item);
   }
-  const item0 = document.querySelector("#item0");
-  const item1 = document.querySelector("#item1");
-  let text = document.createElement("div");
-  let p = document.createElement("p");
-  let h4 = document.createElement("h4");
-  let img = new Image();
-  let price = document.createElement("p");
 
-  price.id = "price";
-  price.textContent = "3$";
-  img.src = img0;
-  item0.appendChild(img);
-  h4.textContent = "Chicken Shawarma In Pita Bread Sandwich";
-  p.textContent =
-    "Chicken shawerma, pickles, French fries and garlic mustard sauce served in pita bread";
-  item0.appendChild(text);
-  text.appendChild(h4);
-  text.appendChild(p);
-  item0.appendChild(price);
-
-  img = new Image();
-  img.src = img1;
-  item1.appendChild(img);
-  h4 = document.createElement("h4");
-  h4.textContent = "";
+  createItem(
+    img0,
+    "4$",
+    "best meal ever",
+    "best des ever",
+    "best-selling-items",
+    "item0"
+  );
 }
